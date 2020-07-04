6837032a322dd38105f7c9066790c55d5010cca9
1200 <1053182739@qq.com>
2020-07-04 13:16:44 +0800

    登录接口2

--- webapp/jxgOps/api/blade-auth/login.ts
+++ webapp/jxgOps/api/blade-auth/login.ts
@@ -1,22 +1,49 @@
-interface params {
+interface HttpConf {
+	methods?: string
+	timeout?: number
+	path: string
+	header?: string[]
+}
+interface Enter {
+	(req: any, res: any, next: Function): any
+}
+interface Outer {
+	(req: any, res: any, next: Function): void
+}
+interface Params {
 	username: string
 	password: string
 	grant_type?: string
 	scope?: string
 	type?: string
 }
-interface login {}
+interface Login {
+	id: string
+	httpConf: HttpConf
+	params: Params
+	enter: Enter
+	outer: Outer
+}
+let params:Params
 const Login = () => {
-	return {
+	let conf:Login = {
 		id: 'kml-auth_login',
-		params: {
-			username: '',
-			password: '',
-			grant_type: 'password',
-			scope: 'all',
-			type: 'account',
+		httpConf: {
+			path: `/${__dirname}/oauth/token/${__filename}`
 		},
-		path: `/${__dirname}/oauth/token/${__filename}`,
+		params,
+		enter (req, res, next) {
+			console.log("kml-auth_login")
+		},
+		outer (req, res, next) {
+			res.body = {
+				code: 200,
+				message: "success",
+				data: ""
+			}
+			return false
+		}
 	}
+	return conf
 }
 exports = module.exports = Login
